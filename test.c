fdf616ccb5225856866b458c7a3162e87fbe3bf4
乌鸦坐飞机 <105417198+CrowsFly5@users.noreply.github.com>
2022-05-16 22:37:20 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,393 +1,470 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+
 #include <stdio.h>
-//EOF
 //
 //int main()
 //{
-//	//
-//	char password[20] = { 0 };
-//	printf("请输入密码:>");
-//	scanf("%s", password);//123456
-//	//gets
-//	printf("请确认密码(Y/N):>");
-//	//清理缓冲区
-//	//getchar();//处理'\n'
-//	// 
-//	//清理缓冲区的中多个字符
-//	int tmp = 0;
-//	while ((tmp = getchar()) != '\n')
-//	{
-//		;
-//	}
-//
-//	int ch = getchar();
-//	if (ch == 'Y')
-//	{
-//		printf("确认成功\n");
-//	}
-//	else
-//	{
-//		printf("确认失败\n");
-//	}
-//	return 0;
-//}
-
-//int main()
-//{
-//	int ch = 0;
-//	while ((ch=getchar()) != EOF)
-//	{
-//		if (ch < '0' || ch>'9')
-//			continue;
-//
-//		putchar(ch);
-//	}
-//	return 0;
-//}
-
-//int main()
-//{
-//	int i = 1;//初始化
-//
-//	while (i <= 10)//判断部分
-//	{
-//		printf("%d ", i);
-//		i++;//调整部分
-//	}
-//
-//	return 0;
-//}
-//
-//int main()
-//{
-//	int i = 0;
-//
-//	for (i = 1; i <= 10; i++)
-//	{
-//		//if (i == 5)
-//		//	break;
-//		if (i == 5)
-//			continue;
-//
-//		printf("%d ", i);
-//	}
-//
-//	return 0;
-//}
-
-//循环体内改变循环变量的例子
-// 
-//int main()
-//{
-//	int i = 0;
-//	for (i = 1; i <= 10; i++)
-//	{
-//		printf("%d ", i);
-//		int j = 0;
-//		for (j = 0; j < 10; i++)
-//		{
-//			printf("hehe\n");
-//		}
-//	}
-//	return 0;
-//}
-
-
-//int main()
-//{
-//	int arr[10] = { 0 };
 //	int i = 0;
 //	for (i = 0; i < 10; i++)
 //	{
-//		printf("%d ", arr[i]);
-//	}
-//	return 0;
-//}
-
-
-//int main()
-//{
-//	//判断部分的省略 - 判断部分恒为真
-//	for (;;)
-//	{
-//		printf("hehe\n");
+//		if (i = 5)
+//			printf("%d ", i);
 //	}
 //	return 0;
 //}
 //
+//
+//
+//int func(int a)
+//{
+//    int b;
+//    switch (a)
+//    {
+//    case 1: b = 30;
+//    case 2: b = 20;
+//    case 3: b = 16;
+//    default: b = 0;
+//    }
+//    return b;
+//}
 //int main()
 //{
-//	int i = 0;
-//	int j = 0;
-//	for (; i < 3; i++)
-//	{
-//		for (; j < 3; j++)
+//    printf("%d\n", func(1));
+//
+//    return 0;
+//}
+
+//F11 - 逐语句
+//F10 - 逐过程
+
+
+//#include <stdio.h>
+//int main() {
+//	int x = 3;
+//	int y = 3;
+//	switch (x % 2) {
+//	case 1:
+//		switch (y)
 //		{
-//			printf("hehe\n");
-//		}
-//	}
-//
-//	return 0;
-//}
-
-//int main()
-//{
-//	int i = 1;
-//	do
-//	{
-//		if (i == 5)
+//		case 0:
+//			printf("first");
+//		case 1:
+//			printf("second");
 //			break;
-//
-//		printf("%d ", i);
-//		i++;
-//	} while (i<=10);
-//
-//	printf("hehe\n");
-//	return 0;
-//}
-
-//while for do while
-
-//n!  - 1->n
-
-//int main()
-//{
-//	int i = 0;
-//	int n = 0;
-//	int ret = 1;
-//	//1*2*3*4*5 = 120
-//	scanf("%d", &n);
-//	for (i = 1; i <= n; i++)
-//	{
-//		ret *= i;
+//		default: printf("hello");
+//		}
+//	case 2:
+//		printf("third");
 //	}
-//	printf("%d\n", ret);
-//
 //	return 0;
 //}
 
-//计算 1!+2!+3!+……+10!
+
+
+//写代码将三个整数数按从大到小输出。
+//
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	//输入
+//	scanf("%d%d%d", &a, &b, &c);
+//	//调整顺序
+//	if (a < b)
+//	{
+//		int tmp = a;
+//		a = b;
+//		b = tmp;
+//	}
+//	if (a < c)
+//	{
+//		int tmp = a;
+//		a = c;
+//		c = tmp;
+//	}
+//	if (b < c)
+//	{
+//		int tmp = b;
+//		b = c;
+//		c = tmp;
+//	}
+//
+//	//输出 - 大到小
+//	printf("%d %d %d\n", a, b, c);
+//	return 0;
+//}
+
+
+//写一个代码打印1 - 100之间所有3的倍数的数字
 //
 //int main()
 //{
 //	int i = 0;
-//	int n = 0;
-//	int ret = 1;
-//	int sum = 0;
-//	//1!+2!+3! = 1+2+6 = 9
-//	//3! = 3*2*1 = 3*2!
-//	//2!=2*1
-//
-//	//for (n = 1; n <= 3; n++)
+//	//for (i = 1; i <= 100; i++)
 //	//{
-//	//	ret = 1;//计算n的阶乘之前，把ret初始为1
-//	//	for (i = 1; i <= n; i++)
+//	//	//判断i是否为3的倍数
+//	//	if (i % 3 == 0)
 //	//	{
-//	//		ret *= i;
+//	//		printf("%d ", i);
 //	//	}
-//	//	sum += ret;
 //	//}
 //
-//	for (n = 1; n <= 3; n++)
+//	for (i = 3; i <= 100; i+=3)
 //	{
-//		ret *= n;
-//		sum += ret;
+//		printf("%d ", i);
 //	}
-//
-//	printf("%d\n", sum);
 //
 //	return 0;
 //}
 
+
+//给定两个数，求这两个数的最大公约数
+
 //
 //int main()
 //{
-//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
-//	int k = 7;//要查找的数字
-//	//在arr这个有序的数组中查找k（7）的值
-//	int sz = sizeof(arr) / sizeof(arr[0]);//数组的元素个数
-//	int left = 0;
-//	int right = sz-1;
-//	while (left<=right)
+//	int m = 0;
+//	int n = 0;
+//	scanf("%d%d", &m, &n);//24 18
+//	int max = 0;
+//	//假设最大公约数max就是m和n的较小值
+//	if (m > n)
+//		max = n;
+//	else
+//		max = m;
+//	
+//	while (1)
 //	{
-//		int mid = (left + right) / 2;
-//		if (arr[mid] < k)
+//		if (m % max == 0 && n % max == 0)
 //		{
-//			left = mid + 1;
-//		}
-//		else if (arr[mid] > k)
-//		{
-//			right = mid - 1;
-//		}
-//		else
-//		{
-//			printf("找到了,下标是:%d\n", mid);
+//			printf("最大公约数就是：%d\n", max);
 //			break;
 //		}
+//		max--;
 //	}
-//	if (left > right)
-//	{
-//		printf("找不到了\n");
-//	}
+//
 //	return 0;
 //}
 //
+//
 
+//辗转相除法
 
-//welcome to bit!!!!!!
-//####################
+//int main()
+//{
+//	int m = 0;
+//	int n = 0;
+//	scanf("%d%d", &m, &n);//24 18
+//	int t = 0;
+//
+//	while (t=m%n)
+//	{
+//		m = n;
+//		n = t;
+//	}
+//	printf("最大公约数：%d\n", n);
+//
+//	//最小公倍数 = m*n/最大公约数;
+//
+//	return 0;
+//}
 
-//w##################!
-//we################!!
-//wel##############!!!
-//...
-//welcome to bit!!!!!!
-//#include <string.h>
-//#include <windows.h>
+//打印1000年到2000年之间的闰年
+
+//int main()
+//{
+//	int y = 0;
+//	int count = 0;
+//	for (y = 1000; y <= 2000; y++)
+//	{
+//		//判断y是不是闰年
+//		//1. 被4整除，不能被100整除是闰年
+//		//2. 能被400整除是闰年
+//		if (y % 4 == 0)
+//		{
+//			if (y % 100 != 0)
+//			{
+//				printf("%d ", y);
+//				count++;
+//			}
+//		}
+//
+//		if (y % 400 == 0)
+//		{
+//			printf("%d ", y);
+//			count++;
+//		}
+//	}
+//	printf("\ncount = %d\n", count);
+//
+//	return 0;
+//}
 //
 //int main()
 //{
-//	char arr1[] = "welcome to bit!!!!!!";
-//	char arr2[] = "####################";
-//	int left = 0;
-//	int right = strlen(arr1)-1;
-//
-//	while (left<=right)
+//	int y = 0;
+//	int count = 0;
+//	for (y = 1000; y <= 2000; y++)
 //	{
-//		arr2[left] = arr1[left];
-//		arr2[right] = arr1[right];
-//		printf("%s\n", arr2);
-//		Sleep(1000);//睡眠1秒
-//		system("cls");//清空屏幕
-//		left++;
-//		right--;
+//		//判断y是不是闰年
+//		//1. 被4整除，不能被100整除是闰年
+//		//2. 能被400整除是闰年
+//		if (((y%4==0) && (y%100!=0)) || (y%400==0))
+//		{
+//			printf("%d ", y);
+//			count++;
+//		}
 //	}
-//	printf("%s\n", arr2);
+//
+//	printf("\ncount = %d\n", count);
 //
 //	return 0;
 //}
-#include <string.h>
-//strlen
-//strcmp
+
+
+
+//写一个代码：打印100~200之间的素数
+//素数 - 质数
+//只能被1和它本身整除
 //
 //int main()
 //{
 //	int i = 0;
-//	//假设正确的密码是字符串"123456"
-//
-//	char password[20] = { 0 };
-//	//abcdef
-//	//abccqqqqqq
-//
-//	for (i = 0; i < 3; i++)
+//	for (i = 100; i <= 200; i++)
 //	{
-//		printf("请输入密码:>");
-//		scanf("%s", password);
-//		//if (password == "123456")//err - 两个字符串比较，不能使用==，应该使用strcmp
-//		if(strcmp(password, "123456") == 0)
+//		//判断i是否为素数
+//		//2->i-1之间的数字去试除i,看能不能整除
+//		int j = 0;
+//		for (j = 2; j < i; j++)
 //		{
-//			printf("登录成功\n");
-//			break;
+//			if (i % j == 0)
+//			{
+//				break;
+//			}
 //		}
-//		else
+//		//
+//		if (i == j)
 //		{
-//			printf("密码错误，重新输入！\n");
+//			printf("%d ", i);//素数
 //		}
 //	}
-//	if (i == 3)
-//		printf("三次密码均错误，退出程序\n");
+//	return 0;
+//}
+
+
+//
+//int main()
+//{
+//	int count = 0;
+//	int i = 0;
+//	for (i = 100; i <= 200; i++)
+//	{
+//		//判断i是否为素数
+//		//2->i-1之间的数字去试除i,看能不能整除
+//		int j = 0;
+//		int flag = 1;//假设i就是素数
+//		for (j = 2; j < i; j++)
+//		{
+//			if (i % j == 0)
+//			{
+//				flag = 0;//不是素数
+//				break;
+//			}
+//		}
+//		//
+//		if (flag == 1)
+//		{
+//			count++;
+//			printf("%d ", i);//素数
+//		}
+//	}
+//	printf("\ncount = %d\n", count);
+//
+//	return 0;
+//}
+//
+//#include <math.h>
+//
+//int main()
+//{
+//	int count = 0;
+//	int i = 0;
+//
+//	//m = a*b
+//	//a和b中一定至少有一个数字是 <= 开平方m的
+//	//16 = 2*8 = 4*4
+//
+//	//sqrt - 是开平方的函数 - math.h
+//	for (i = 100; i <= 200; i++)
+//	{
+//		//判断i是否为素数
+//		//2->i-1之间的数字去试除i,看能不能整除
+//		int j = 0;
+//		int flag = 1;//假设i就是素数
+//		for (j = 2; j <= sqrt(i); j++)
+//		{
+//			if (i % j == 0)
+//			{
+//				flag = 0;//不是素数
+//				break;
+//			}
+//		}
+//		//
+//		if (flag == 1)
+//		{
+//			count++;
+//			printf("%d ", i);//素数
+//		}
+//	}
+//	printf("\ncount = %d\n", count);
 //
 //	return 0;
 //}
 
+
 //
-//写一个猜数字游戏
-//1. 自动产生一个1-100之间的随机数
-//2. 猜数字
-//  a. 猜对了，就恭喜你，游戏结束
-//  b. 你猜错了，会告诉猜大了，还是猜小了，继续猜，直到猜对
-//3. 游戏一个一直玩，除非退出游戏
+//#include <math.h>
 //
-//
-//#include <stdlib.h>
-//#include <time.h>
-//
-//
-//void menu()
+//int main()
 //{
-//	printf("*****************************\n");
-//	printf("*******  1. play     ********\n");
-//	printf("*******  0. exit     ********\n");
-//	printf("*****************************\n");
+//	int count = 0;
+//	int i = 0;
+//
+//	//m = a*b
+//	//a和b中一定至少有一个数字是 <= 开平方m的
+//	//16 = 2*8 = 4*4
+//
+//	//sqrt - 是开平方的函数 - math.h
+//	for (i = 101; i <= 200; i+=2)
+//	{
+//		//判断i是否为素数
+//		//2->i-1之间的数字去试除i,看能不能整除
+//		int j = 0;
+//		int flag = 1;//假设i就是素数
+//		for (j = 2; j <= sqrt(i); j++)
+//		{
+//			if (i % j == 0)
+//			{
+//				flag = 0;//不是素数
+//				break;
+//			}
+//		}
+//		if (flag == 1)
+//		{
+//			count++;
+//			printf("%d ", i);//素数
+//		}
+//	}
+//	printf("\ncount = %d\n", count);
+//
+//	return 0;
 //}
-////
-//void game()
+//
+
+
+//int main()
 //{
-//	//猜数字游戏的实现
-//	//1. 生成随机数
-//	//rand函数返回了一个0-32767之间的数字
-//	//时间 - 时间戳
+//flag:
+//	printf("hehe\n");
+//	printf("haha\n");
 //
+//	goto flag;
+//	return 0;
+//}
+
+//关机程序
+//只要运行起来，电脑就在1分钟内关机，如果输入：我是猪，就取消关机!
 //
-//	int ret = rand()%100+1;//%100的余数是0-99，然后+1，范围就是1-100
-//	//printf("%d\n", ret);
+//shutdown -s -t 60
+//shutdown -a
 //
-//	//2. 猜数字
-//	int guess = 0;
+#include <string.h>
+#include <stdlib.h>
+//
+//int main()
+//{
+//	//关机
+//	//C语言提供了一个函数：system()- 执行系统命令的
+//
+//	char input[20] = { 0 };//存放输入的信息
+//	system("shutdown -s -t 60");//system - stdlib.h
+//
+//again:
+//	printf("请注意，你的电脑在1分钟内关机，如果输入：我是猪，就取消关机\n");
+//	scanf("%s", input);
+//	//string.h
+//	if (strcmp(input, "我是猪") == 0)//两个字符串比较是不能使用==的，应该使用strcmp() string compare
+//	{
+//		system("shutdown -a");
+//	}
+//	else
+//	{
+//		goto again;
+//	}
+//	return 0;
+//}
+
+//
+//int main()
+//{
+//	//关机
+//	//C语言提供了一个函数：system()- 执行系统命令的
+//	char input[20] = { 0 };//存放输入的信息
+//	system("shutdown -s -t 60");//system - stdlib.h
 //	while (1)
 //	{
-//		printf("请猜数字:>");
-//		scanf("%d", &guess);
-//		if (guess < ret)
+//		printf("请注意，你的电脑在1分钟内关机，如果输入：我是猪，就取消关机\n");
+//		scanf("%s", input);
+//		//string.h
+//		if (strcmp(input, "我是猪") == 0)//两个字符串比较是不能使用==的，应该使用strcmp() string compare
 //		{
-//			printf("猜小了\n");
-//		}
-//		else if (guess > ret)
-//		{
-//			printf("猜大了\n");
-//		}
-//		else
-//		{
-//			printf("恭喜你，猜对了\n");
+//			system("shutdown -a");
 //			break;
 //		}
 //	}
+//
+//	return 0;
+//}
+
+
+//goto 语句只能在一个函数范围内跳转，不能跨函数
+//
+//void test()
+//{
+//flag:
+//	printf("test\h");
 //}
 //
-//int  main()
+//int main()
 //{
-//	int input = 0;
-//	srand((unsigned int)time(NULL));
+//	goto flag;
+//	return 0;
+//}
+
+
+//f(x) = 2*x+1;
+//f(x, y) = x+y;
+#include <string.h>
+
+//int main()
+//{
+//	char arr1[20] = { 0 };
+//	char arr2[] = "hello bit";
+//	strcpy(arr1, arr2);
+//	printf("%s", arr1);//打印arr1这个字符串 %s - 以字符串的格式打印
 //
-//	do
-//	{
-//		menu();//打印菜单
-//		printf("请选择:>");
-//		scanf("%d", &input);
-//		switch (input)
-//		{
-//		case 1:
-//			game();
-//			break;
-//		case 0:
-//			printf("退出游戏\n");
-//			break;
-//		default:
-//			printf("选择错误,重新选择\n");
-//			break;
-//		}
-//
-//	} while (input);
 //	return 0;
 //}
 //
-
-#include <stdio.h>
 
 int main()
 {
-	char arr[10] = {0};
-	gets(arr);
-	puts(arr);
+	char arr[] = "hello bit";
+	memset(arr, 'x', 5);
+	
+	printf("%s\n", arr);
+
 	return 0;
 }
+
